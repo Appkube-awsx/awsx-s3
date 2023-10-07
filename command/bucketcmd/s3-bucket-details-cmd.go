5115b660d658344178d99e831d8d43d1165706f8
ManojSynectiks <manoj.sharma@synectiks.com>
2023-10-07 13:54:57 +0530

    code refactoring and separate tag api

--- command/bucketcmd/s3-bucket-details-cmd.go
+++ command/bucketcmd/s3-bucket-details-cmd.go
@@ -1,6 +1,7 @@
 package bucketcmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Appkube-awsx/awsx-common/authenticate"
 	"log"
@@ -10,6 +11,11 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/spf13/cobra"
 )
+
+type S3Bucket struct {
+	Bucket interface{} `json:"bucket"`
+	Tags   interface{} `json:"tags"`
+}
 
 // GetConfigDataCmd represents the getConfigData command
 var GetConfigDataCmd = &cobra.Command{
@@ -26,7 +32,7 @@ var GetConfigDataCmd = &cobra.Command{
 		if authFlag {
 			bucketName, _ := cmd.Flags().GetString("bucketName")
 			if bucketName != "" {
-				GetBucketDetails(bucketName, *clientAuth)
+				GetBucketDetail(bucketName, *clientAuth)
 			} else {
 				log.Fatalln("bucket name not provided. program exit")
 			}
@@ -35,20 +41,71 @@ var GetConfigDataCmd = &cobra.Command{
 	},
 }
 
-func GetBucketDetails(bucketName string, auth client.Auth) (*s3.ListObjectsV2Output, error) {
-	log.Println("Getting aws bucket data")
-	listbucketClient := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
+func GetBucketDetail(bucketName string, auth client.Auth) (*s3.ListObjectsV2Output, error) {
+	log.Println("Getting details of bucket: ", bucketName)
+	client := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
 	input := &s3.ListObjectsV2Input{
 		Bucket: aws.String(bucketName),
 	}
-	bucketDetailsResponse, err := listbucketClient.ListObjectsV2(input)
-	log.Println(bucketDetailsResponse.String())
+	response, err := client.ListObjectsV2(input)
 	if err != nil {
 		log.Println("Error:", err)
 		return nil, err
 	}
+	log.Println(response)
+	return response, nil
+}
 
-	return bucketDetailsResponse, nil
+func GetBucketDetailWithTags(bucketName string, auth client.Auth) (string, error) {
+	log.Println("getting s3 bucket with tags. bucket: ", bucketName)
+	bucketDetail, err := GetBucketDetail(bucketName, auth)
+	if err != nil {
+		log.Println("Error in getting bucket detail", err)
+		return "", err
+	}
+	client := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
+	input := &s3.GetBucketTaggingInput{
+		Bucket: aws.String(bucketName),
+	}
+	tagOutput, err := client.GetBucketTagging(input)
+	if err != nil {
+		log.Println("Error in getting bucket tag: ", err)
+		return "", err
+	}
+	s3Bucket := S3Bucket{
+		Bucket: bucketDetail,
+		Tags:   tagOutput,
+	}
+	jsonData, err := json.Marshal(s3Bucket)
+	if err != nil {
+		log.Println("Error in getting bucket tag", err)
+		return "", err
+	}
+	log.Println(string(jsonData))
+	return string(jsonData), err
+}
+
+func GetBucketListWithDetails(auth client.Auth) ([]*s3.ListObjectsV2Output, error) {
+	log.Println("getting s3 bucket list with details")
+	client := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
+	request := &s3.ListBucketsInput{}
+	response, err := client.ListBuckets(request)
+	if err != nil {
+		log.Println("Error:in getting  bucket list", err)
+		return nil, err
+	}
+
+	allBuckets := []*s3.ListObjectsV2Output{}
+	for _, bucket := range response.Buckets {
+		bucketDetail, err := GetBucketDetail(*bucket.Name, auth)
+		if err != nil {
+			log.Println("Error in getting bucket detail ", err)
+			continue
+		}
+		allBuckets = append(allBuckets, bucketDetail)
+	}
+	log.Println(allBuckets)
+	return allBuckets, err
 }
 
 func init() {
--- controller/s3-controller.go
+++ controller/s3-controller.go
@@ -6,20 +6,21 @@ import (
 	"github.com/Appkube-awsx/awsx-common/client"
 	"github.com/Appkube-awsx/awsx-s3/command"
 	"github.com/Appkube-awsx/awsx-s3/command/bucketcmd"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"log"
 )
 
-func GetS3BucketListByAccountNo(vaultUrl string, vaultToken string, accountNo string, region string) ([]*s3.ListObjectsV2Output, error) {
+func GetS3BucketListByAccountNo(vaultUrl string, vaultToken string, accountNo string, region string) (*s3.ListBucketsOutput, error) {
 	authFlag, clientAuth, err := authenticate.AuthenticateData(vaultUrl, vaultToken, accountNo, region, "", "", "", "")
 	return GetS3BucketListByFlagAndClientAuth(authFlag, clientAuth, err)
 }
 
-func GetS3BucketListByUserCreds(region string, accessKey string, secretKey string, crossAccountRoleArn string, externalId string) ([]*s3.ListObjectsV2Output, error) {
+func GetS3BucketListByUserCreds(region string, accessKey string, secretKey string, crossAccountRoleArn string, externalId string) (*s3.ListBucketsOutput, error) {
 	authFlag, clientAuth, err := authenticate.AuthenticateData("", "", "", region, accessKey, secretKey, crossAccountRoleArn, externalId)
 	return GetS3BucketListByFlagAndClientAuth(authFlag, clientAuth, err)
 }
-func GetS3BucketListByFlagAndClientAuth(authFlag bool, clientAuth *client.Auth, err error) ([]*s3.ListObjectsV2Output, error) {
+func GetS3BucketListByFlagAndClientAuth(authFlag bool, clientAuth *client.Auth, err error) (*s3.ListBucketsOutput, error) {
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
@@ -28,7 +29,7 @@ func GetS3BucketListByFlagAndClientAuth(authFlag bool, clientAuth *client.Auth, 
 		log.Println(err.Error())
 		return nil, err
 	}
-	response, err := GetS3BucketList(clientAuth)
+	response, err := S3BucketListController(*clientAuth)
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
@@ -36,8 +37,9 @@ func GetS3BucketListByFlagAndClientAuth(authFlag bool, clientAuth *client.Auth, 
 	return response, nil
 }
 
-func GetS3BucketList(clientAuth *client.Auth) ([]*s3.ListObjectsV2Output, error) {
-	response, err := command.GetListBucketWithBucketDetail(*clientAuth)
+func S3BucketListController(clientAuth client.Auth) (*s3.ListBucketsOutput, error) {
+	log.Println("Request to get s3 bucket list")
+	response, err := command.GetBucketList(clientAuth)
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
@@ -45,23 +47,80 @@ func GetS3BucketList(clientAuth *client.Auth) ([]*s3.ListObjectsV2Output, error)
 	return response, nil
 }
 
-func GetBucketList(clientAuth *client.Auth) (string, error) {
-	response, err := command.GetBucketList(*clientAuth)
+func S3BucketListWithDetailsController(clientAuth client.Auth) ([]*s3.ListObjectsV2Output, error) {
+	log.Println("Request to get s3 bucket list with details")
+	response, err := bucketcmd.GetBucketListWithDetails(clientAuth)
 	if err != nil {
 		log.Println(err.Error())
+		return nil, err
+	}
+	return response, nil
+}
+
+func S3BucketDetailController(bucketName string, clientAuth client.Auth) (*s3.ListObjectsV2Output, error) {
+	log.Println("Request to get detail of bucket: ", bucketName)
+	response, err := bucketcmd.GetBucketDetail(bucketName, clientAuth)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	return response, nil
+}
+
+func S3BucketListWithTagsController(auth client.Auth) (string, error) {
+	log.Println("getting s3 bucket list with tags")
+	response, err := command.GetBucketList(auth)
+	if err != nil {
+		log.Println("Error:in getting  bucket list", err)
 		return "", err
 	}
-	jsonData, err := json.Marshal(response)
+	client := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
+	allBuckets := []bucketcmd.S3Bucket{}
+	for _, bucket := range response.Buckets {
+		input := &s3.GetBucketTaggingInput{
+			Bucket: bucket.Name,
+		}
+		tagOutput, err := client.GetBucketTagging(input)
+		if err != nil {
+			log.Println("Error in getting bucket tag", err)
+			continue
+		}
+		s3Bucket := bucketcmd.S3Bucket{
+			Bucket: bucket,
+			Tags:   tagOutput,
+		}
+		allBuckets = append(allBuckets, s3Bucket)
+	}
+	jsonData, err := json.Marshal(allBuckets)
 	log.Println(string(jsonData))
 	return string(jsonData), err
-	//return response, nil
 }
 
-func GetS3BucketDetails(bucketName string, clientAuth *client.Auth) (*s3.ListObjectsV2Output, error) {
-	response, err := bucketcmd.GetBucketDetails(bucketName, *clientAuth)
+func S3BucketWithTagsController(bucketName string, auth client.Auth) (string, error) {
+	log.Println("getting s3 bucket with tags. bucket: ", bucketName)
+	bucketDetail, err := S3BucketDetailController(bucketName, auth)
 	if err != nil {
-		log.Println(err.Error())
-		return nil, err
+		log.Println("Error in getting bucket detail", err)
+		return "", err
 	}
-	return response, nil
+	client := client.GetClient(auth, client.S3_CLIENT).(*s3.S3)
+	input := &s3.GetBucketTaggingInput{
+		Bucket: aws.String(bucketName),
+	}
+	tagOutput, err := client.GetBucketTagging(input)
+	if err != nil {
+		log.Println("Error in getting bucket tag: ", err)
+		return "", err
+	}
+	s3Bucket := bucketcmd.S3Bucket{
+		Bucket: bucketDetail,
+		Tags:   tagOutput,
+	}
+	jsonData, err := json.Marshal(s3Bucket)
+	if err != nil {
+		log.Println("Error in json marshal of bucket: ", err)
+		return "", err
+	}
+	log.Println(string(jsonData))
+	return string(jsonData), err
 }
